af79dc011ee789a1020f16738486e5af96ca2701
shipcommit <fredrik.a@protonmail.ch>
2019-02-26 18:48:09 +0200

    Role based authorization works

--- users/user.model.js
+++ users/user.model.js
@@ -19,6 +19,10 @@ const schema = new Schema({
     type: String,
     required: true
   },
+  role: {
+    type: String,
+    default: 'User'
+  },
   createdDate: {
     type: Date,
     default: Date.now
